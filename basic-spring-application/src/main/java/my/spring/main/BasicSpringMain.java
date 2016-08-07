a691a22f4f608dbb146be3dcf9aa91ce955937ee
Shashikant Mokkarala <shashikant.mokkarala@outlook.com>
2016-08-07 19:13:54 +0530

    Added files to load the application context.

---
+++ basic-spring-application/src/main/java/my/spring/main/BasicSpringMain.java
@@ -0,0 +1,15 @@
+package my.spring.main;
+
+import org.springframework.context.ApplicationContext;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+
+public class BasicSpringMain {
+
+	@SuppressWarnings("resource")
+	public static void main(String[] args) {
+		ApplicationContext springContext = new ClassPathXmlApplicationContext("application-beans.xml");
+		MyBean myBean = (MyBean)springContext.getBean("myBean");
+		System.out.println(myBean.sayHello());
+	}
+
+}
---
+++ basic-spring-application/src/main/java/my/spring/main/MyBean.java
@@ -0,0 +1,9 @@
+package my.spring.main;
+
+public class MyBean {
+
+	public String sayHello(){
+		return "Hello Guys...!!";
+	}
+	
+}
